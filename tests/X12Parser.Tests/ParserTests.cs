4670e614b3a305e9bf4f873e4e1757b996fbea1a
Ken Wilcox <wilcoxk@medamerica.com>
2021-12-10 09:12:58 -0700

    This should fix #15

--- tests/X12Parser.Tests/ParserTests.cs
+++ tests/X12Parser.Tests/ParserTests.cs
@@ -8,17 +8,18 @@ namespace X12Parser.Tests
 {
     public class ParserTests
     {
-        private X12Factory _factory;
-        private List<X12> _sut;
-        private List<PropCache> _cache;
+        private readonly X12Factory _factory;
+        private readonly List<X12> _sut;
+        private readonly List<PropCache> _cache;
 
+        // ReSharper disable CommentTypo
         //private const string _isa = @"ISA*00*AAAAAAAAAA*11*BBBBBBBBBB*22*123456789012345*33*987654321987654*CCCCCC*DDDD*^*EEEEE*FFFFFFFFF*G*T*:~";
-        private const string _isaProperties = @"ISA*AA*BBBBBBBBBB*CC*DDDDDDDDDD*EE*FFFFFFFFFFFFFFF*GG*HHHHHHHHHHHHHHH*IIIIII*JJJJ*K*LLLLL*MMMMMMMMM*N*O*P~";
-        private const string _optionalFoo = @"FOO*REQUIRED*~";
-        private const string _onlyOptional = @"OPT*****HERE~";
-        private const string _minTest = "MIN*123456789~";
-        private const string _maxTest = "MAX*12345678901234567890~";
-        private const string _isaWithCrLf = @"ISA*00*          *00*          *ZZ*610442         *ZZ*0FB            *190326*2111*|*00
+        private const string IsaProperties = @"ISA*AA*BBBBBBBBBB*CC*DDDDDDDDDD*EE*FFFFFFFFFFFFFFF*GG*HHHHHHHHHHHHHHH*IIIIII*JJJJ*K*LLLLL*MMMMMMMMM*N*O*P~";
+        private const string OptionalFoo = @"FOO*REQUIRED*~";
+        private const string OnlyOptional = @"OPT*****HERE~";
+        private const string MinTest = "MIN*123456789~";
+        private const string MaxTest = "MAX*12345678901234567890~";
+        private const string IsaWithCrLf = @"ISA*00*          *00*          *ZZ*610442         *ZZ*0FB            *190326*2111*^*00
 001*
 000000002*0*P*>~";
 
@@ -28,7 +29,7 @@ namespace X12Parser.Tests
             // which makes the call to GetEntryAssembly return null, this way we are handing
             // "ourselves" to the factory instead of the factory trying to find us.
             _factory = new X12Factory(typeof(X12), Assembly.GetExecutingAssembly());
-            _sut = Parser.ParseText(_isaProperties, _factory);
+            _sut = Parser.ParseText(IsaProperties, _factory);
             _cache = _factory.GetPropertiesForType("ISA");
         }
 
@@ -51,6 +52,8 @@ namespace X12Parser.Tests
             {
                 var info = _cache.FirstOrDefault(x => x.Segment.Order == i);
                 Assert.NotNull(info);
+                Assert.NotNull(info.Segment);
+                Assert.NotNull(info.Segment.MaxLength);
                 var c = Convert.ToChar(startAt);
                 var test = c.ToString().PadLeft(info.Segment.MaxLength.Value, c);
                 Assert.Equal(test, info.Property.GetValue(sut));
@@ -62,8 +65,9 @@ namespace X12Parser.Tests
         [Fact]
         public void TestThat_OptionalFields_WorkAsExpected()
         {
-            var sut = Parser.ParseText(_optionalFoo, _factory);
+            var sut = Parser.ParseText(OptionalFoo, _factory);
             var foo = sut.FirstOrDefault() as FOO;
+            Assert.NotNull(foo);
             Assert.Equal("FOO", foo.RecordType);
             Assert.Equal("REQUIRED", foo.FieldOne);
             Assert.Equal("", foo.OptionalOne);
@@ -73,8 +77,9 @@ namespace X12Parser.Tests
         [Fact]
         public void TestThat_OnlyOptionalFields_WorkAsExpected()
         {
-            var sut = Parser.ParseText(_onlyOptional, _factory);
+            var sut = Parser.ParseText(OnlyOptional, _factory);
             var opt = sut.FirstOrDefault() as OPT;
+            Assert.NotNull(opt);
             Assert.Equal("OPT", opt.RecordType);
             Assert.Equal("HERE", opt.Here);
         }
@@ -82,13 +87,13 @@ namespace X12Parser.Tests
         [Fact]
         public void TestThat_MinLength_IsChecked()
         {
-            Assert.Throws<ArgumentException>(() => Parser.ParseText(_minTest, _factory));
+            Assert.Throws<ArgumentException>(() => Parser.ParseText(MinTest, _factory));
         }
 
         [Fact]
         public void TestThat_MaxLength_IsChecked()
         {
-            Assert.Throws<ArgumentException>(() => Parser.ParseText(_maxTest, _factory));
+            Assert.Throws<ArgumentException>(() => Parser.ParseText(MaxTest, _factory));
         }
 
         [Fact]
@@ -96,8 +101,9 @@ namespace X12Parser.Tests
         {
             // The library does not handle this anymore. There were way too many unexpected
             // characters in the 835 files I've parsed.
-            var sut = Parser.ParseText(_isaWithCrLf.Replace("\r\n", "").Replace("\n", ""), _factory);
+            var sut = Parser.ParseText(IsaWithCrLf.Replace("\r\n", "").Replace("\n", ""), _factory);
             var opt = sut.FirstOrDefault() as Segments.ISA;
+            Assert.NotNull(opt);
             Assert.Equal("ISA", opt.RecordType);
             // new line in value
             Assert.Equal("00001", opt.InterchangeControlVersionNumber);
