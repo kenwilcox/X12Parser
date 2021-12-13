889fb3e03373860e8c850bcab1606e5ec8472168
Ken Wilcox <wilcoxk@gmail.com>
2021-12-13 13:17:18 -0700

    just checking everything in (#17)

--- src/X12Tester/Program.cs
+++ src/X12Tester/Program.cs
@@ -1,15 +1,18 @@
 ﻿using System;
+using System.Linq;
 using System.IO;
 using X12Parser;
+using System.Collections.Generic;
 
 namespace X12Tester
 {
     public static class Program
     {
-        public static void Main(string[] args)
+        public static void Main()//string[] args)
         {
-            Parse835Files(args);
-            //Parse277File();
+            Parse835Files();
+            Parse277File();
+            ParsePLBFiles();
 
 #if DEBUG
             if (!System.Diagnostics.Debugger.IsAttached) return;
@@ -19,15 +22,72 @@ namespace X12Tester
         }
         private static void Parse277File()
         {
-            const string fileName = @"C:\Dev\Projects\N091515.277";
+            var fileName = TesterSettings.Test277;
             var segments = Parser.Parse(fileName);
             segments.ForEach(x => x.Dump($"{Environment.NewLine} *** {x.RecordType}  ***"));
         }
 
-        private static void Parse835Files(string[] args)
+        private static void ParsePLBFiles()
         {
-            var inputPath = @"C:\temp\835";
-            var outputPath = @"C:\temp\835_parsed";
+            var items = File.ReadAllLines(TesterSettings.ProcessCsv).Skip(1);
+            var factory = new X12Factory(typeof(X12));
+            var outputPath = TesterSettings.PlbOutput;
+            var max = 0;
+            var dates = new List<string>();
+
+            foreach (var item in items)
+            {
+                var parts = item.Split(',');
+                var file = parts[0];
+                var count = int.Parse(parts[1]);
+
+                var payer = file.Split('\\')[5];
+                var newPath = Path.Combine(outputPath, payer);
+                var newFile = file.Replace(Path.GetDirectoryName(file), newPath) + ".txt";
+                Console.WriteLine($"{file} - {newFile}");
+
+                var segments = Parser.Parse(file, factory, false);
+                var plbs = segments.Where(x => x.RecordType == "PLB").ToList();
+                if (plbs.Count != count) throw new Exception("Didn't get what we expected");
+
+                //Directory.CreateDirectory(newPath);
+                //using (StreamWriter writer = File.CreateText(newFile))
+                //{
+                //    //segments.ForEach(x => x.Dump(writer, $"{Environment.NewLine} *** {x.RecordType}  ***"));
+                //    //segments.Where(x => x.RecordType == "PLB" || x.RecordType == "TRN" || x.RecordType == "BPR").ToList().ForEach(x => x.Dump(writer, $"{Environment.NewLine} *** {x.RecordType}  ***"));
+                //    segments.Where(x => x.RecordType == "PLB").ToList().ForEach(x => x.Dump(writer, $"{Environment.NewLine} *** {x.RecordType}  ***"));
+                //    writer.Flush();
+                //}
+
+                // find max - testing custom properties
+                // if (plb is PLB p) - borowed from other languages? Swift?
+                foreach (var plb in plbs.Where(x => x is PLB).Select(x => x as PLB))
+                {
+                    var pMax = plb.ReferenceIdentification.Length;
+                    if (pMax > max) max = pMax;
+
+                    if (!plb.Date.EndsWith("1231")) dates.Add(plb.Date);
+                    if (plb.AdjustmentReasonCode1.Length < 2)
+                    {
+                        Console.WriteLine("Stop");
+                    }
+
+                    //foreach(var section in plb.Sections)
+                    //{
+                    //    Console.WriteLine(section);
+                    //}
+                }
+            }
+
+            Console.WriteLine($"Max = {max}");
+            Console.WriteLine($"Date Count: {dates.Count}");
+            Console.WriteLine($"Date Count: {dates.Distinct().Count()}");
+        }
+
+        private static void Parse835Files()
+        {
+            var inputPath = TesterSettings.InputPath;
+            var outputPath = Path.Combine(inputPath, "_parsed");
             Directory.CreateDirectory(outputPath);
             var start = DateTime.Now;
             var counter = 0;
@@ -52,6 +112,8 @@ namespace X12Tester
             }
             var end = DateTime.Now;
             var total = end - start;
+            var dummy = factory.GetPropertiesForType("ISA");
+            Console.WriteLine($"ISA has {dummy.Count()} properties");
             Console.WriteLine($"*** DONE {counter} files in {total} ***");
         }
     }
